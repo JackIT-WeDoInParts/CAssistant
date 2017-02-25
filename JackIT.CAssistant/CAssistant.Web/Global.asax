f76157da855a74e6d438de5e73742d06d402f196
Vitor Davoglio <Vitor Davoglio>
2017-02-25 00:43:09 -0300

    Project Initial Commit Uploaded

---
+++ JackIT.CAssistant/CAssistant.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CAssistant.Web.MvcApplication" Language="C#" %>
---
+++ JackIT.CAssistant/CAssistant.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CAssistant.Web.Startup))]
+namespace CAssistant.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
